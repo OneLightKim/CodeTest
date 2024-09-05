88a34c1f4a5b846d67ffd08d0e392894f8791d2b
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2024-09-05 23:30:49 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,21 @@
+def solution(price):
+    # answer = 0
+    # if 300000>price>=100000:
+    #     answer=round(price*0.95)
+    # elif 500000>price>=300000:
+    #     answer=round(price*0.9)
+    # elif price>=500000:
+    #     answer=round(price*0.8)
+    # else:
+    #     answer=price
+    # return answer
+    if 500000 <= price:
+        price *= 0.80
+    
+    elif 300000 <= price < 500000:
+        price *= 0.90
+        
+    elif 100000 <= price:
+        price *= 0.95
+        
+    return int(price)
