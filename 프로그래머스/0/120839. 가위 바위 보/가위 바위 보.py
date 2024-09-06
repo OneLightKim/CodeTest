0d70652f52ffde99718d872b96018363cc72f629
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2024-09-06 20:17:12 +0900

    [level 0] Title: 가위 바위 보, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,11 @@
+def solution(rsp):
+    answer = ''
+    for i in rsp:
+        i = int(i)
+        if i == 0:
+            answer += '5'
+        elif i == 2:
+            answer += '0'
+        elif i == 5:
+            answer += '2'
+    return answer
