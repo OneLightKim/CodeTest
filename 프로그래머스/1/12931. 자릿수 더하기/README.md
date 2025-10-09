4043e5e7e93977fd5e913523215d9d5450a78134
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2025-10-10 01:43:50 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/README.md
+++ 프로그래머스/1/12931. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자릿수 더하기 - 12931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.15 MB, 시간: 0.01 ms
+메모리: 9.14 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 31일 22:50:54
+2025년 10월 10일 01:43:46
 
 ### 문제 설명
 
--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,8 +1,33 @@
+# def solution(n):
+#     answer = 0
+#     n_str = str(n)
+#     num_len = len(n_str)
+#     for i in range(1,num_len+1):
+#         digit = (n//(10**(num_len-i))) %10
+#         answer += digit
+#     return answer
+
+
+# def solution(n):
+#     answer = 0
+#     n_str = str(n)
+#     num_len = len(n_str)
+#     print(num_len)
+#     print()
+#     answer = 0
+#     for i in range(1,num_len+1):
+#         answer += n//(10**(num_len-i))
+#         n -= (10**(num_len-i))*(n//(10**(num_len-i))%10)
+        
+#         print(f"몫 answer: {answer}")
+#         print(f"자리값 answer*(10**(num_len-i)): {answer*(10**(num_len-i))}")
+#         print(f"n:{n}")
+#     return answer
+# solution(123)
+
 def solution(n):
-    answer = 0
-    n_str = str(n)
-    num_len = len(n_str)
-    for i in range(1,num_len+1):
-        digit = (n//(10**(num_len-i))) %10
-        answer += digit
-    return answer
+    sum=0
+    while n>0:
+        sum += n%10
+        n //= 10
+    return sum
