0b1b4e70f3e260f2f73f41667d8838d37ffb9fb3
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2025-10-19 12:46:39 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 8.98 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    count = 0
+    d.sort()
+    for i in range(len(d)):
+        if budget-d[i]>=0:
+            budget -= d[i]
+            count += 1
+        else:
+            break
+    return count
