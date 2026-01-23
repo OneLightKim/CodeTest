0e06613692201417472761b3944a347614fcb7db
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-01-23 14:04:08 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 60.44 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -0,0 +1,22 @@
+#[PCCE 기출문제] 10번 / 공원
+#mats를 sort
+def solution(mats, park):
+    answer = 0
+    # 필 수 있는 가장 큰 돗자리를 구해야함.
+    # mat * mat로 구할 수 있는 곳 끝까지 구해야할듯
+
+    mats.sort(reverse=True)
+    
+    for mat in mats:
+        #index가 초과되지 않게, mat길이만큼
+        for r in range(len(park)-mat+1):
+            for c in range(len(park[r])-mat+1):
+                count = 0
+                for i in range(mat):
+                    for j in range(mat):
+                        if park[r+i][c+j] != '-1':
+                            count += 1
+                if count == 0:
+                    return mat
+            
+    return -1
