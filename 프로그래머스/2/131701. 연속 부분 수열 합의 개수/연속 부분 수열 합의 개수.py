ccc3fce7bfa8ac1b86367e2e59ca079a7680a580
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-07-08 18:49:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2968.53 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -2,14 +2,9 @@
 #Sliding window
 def solution(elements):
     answer = set()
-    #길이가 전체가 아닐 때에는 그냥 길이만큼 반복문이 도나?
-        # O
-    
-    elements = elements*2
+    elements = elements * 2
     for i in range(len(elements)//2):
-        #마지막의 경우 그냥 elements 전체 더해버리기
         for j in range(len(elements)//2):
-            # index out of range를 해결할 방법? 그냥 elements두배 하면 되는 일. 
             answer.add(sum(elements[j:j+i+1]))
     return len(answer)
 """
