619f66675f37204e9ec61ef77763827f2de24bea
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-08-03 17:48:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.03 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,29 @@
+# def solution(participant, completion):
+#     hash_dict = {}
+#     for p in participant:
+#         if p in hash_dict:
+#             hash_dict[p] += 1
+#         else:
+#             hash_dict[p] = 1
+#     for p in completion:
+#         hash_dict[p] -= 1
+#     for key, value in hash_dict.items():
+#         if value > 0:
+#             return key
+
+
 def solution(participant, completion):
-    hash_dict = {}
+    h_dict = {}
     for p in participant:
-        if p in hash_dict:
-            hash_dict[p] += 1
-        else:
-            hash_dict[p] = 1
-    for p in completion:
-        hash_dict[p] -= 1
-    for key, value in hash_dict.items():
+        if p not in h_dict:
+            h_dict[p] = 0
+        h_dict[p] += 1
+    for c in completion:
+        if c in h_dict.keys():
+            h_dict[c] -= 1
+    for key, value in h_dict.items():
         if value > 0:
             return key
+        
+        
+        
