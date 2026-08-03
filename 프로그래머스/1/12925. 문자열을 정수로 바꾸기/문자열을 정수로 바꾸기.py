327997446877d5bd9d3f3beaf281b56674fac648
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-08-03 17:35:38 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -1,2 +1,22 @@
+# def solution(s):
+#     result = 0
+#     for idx, number in enumerate(s[::-1]):
+#         if number == '-':
+#             result *= -1
+#         else:
+#             result += int(number) * (10**idx)
+        
+#     return result
+
+
+
 def solution(s):
-    return int(s)
+    S = list(s)
+    n = 1
+    for i in range(len(S)):
+        if S[i] == '-':
+            del S[i]
+            n = -1
+            break
+    return int(''.join(S))*n
+            
