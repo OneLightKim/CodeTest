1a46da1ef962b2c7f74aaea9c9a8ae2b1ec6bb7e
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-08-04 21:23:05 +0900

    [level 2] Title: 올바른 괄호, Time: 6.90 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,34 @@
+# def solution(s):
+#     a, b = 0, 0
+#     for i in s:
+#         if i == '(':
+#             a += 1
+#         elif i == ')' and a == 0:
+#             return False
+#         elif i == ')' and a != 0:
+#             b += 1
+#         if b > a:
+#             return False
+#     if a == b:
+#         return True
+#     else:
+#         return False
+
+
+
+
+
+
 def solution(s):
-    a, b = 0, 0
-    for i in s:
-        if i == '(':
-            a += 1
-        elif i == ')' and a == 0:
-            return False
-        elif i == ')' and a != 0:
-            b += 1
-        if b > a:
-            return False
-    if a == b:
-        return True
-    else:
+    if s[0] == ')' or s[-1] == '(':
         return False
+    else:
+        count = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                count += 1
+            else:
+                count -= 1
+            if count < 0:
+                return False
+        return count == 0 
