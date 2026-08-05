23ff12f12e3e4ce53b8ebdd91bef5a2e20bccdfe
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-08-05 17:20:38 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,2 +1,7 @@
+# def solution(s):
+#     return ''.join(sorted(s, reverse=True))
+
+
 def solution(s):
-    return ''.join(sorted(s, reverse=True))
+    answer = ''.join(sorted(s, reverse=True)) #sorted는 항상 리스트로 반환함. 그래서 join이 필요. 근데, 정수들은 결합이 안되니, 결합을 하려하면 map으로 str로 바꾸고 결합하도록 강제해야함
+    return answer
