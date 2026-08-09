d9a2d5156ae8dd7f2a9cff4af42417b7b5a47ce1
OneLightKim <118673375+OneLightKim@users.noreply.github.com>
2026-08-09 15:53:50 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,23 +1,55 @@
-def solution(n, lost, reserve):
-    # 1. 중복 제거 (여벌 있는데 도난당한 애들은 양쪽 명단에서 삭제)
-    # set을 쓰면 자동으로 정렬되지 않으므로 나중에 list로 변환 및 정렬 필요
-    real_reserve = set(reserve) - set(lost)
-    real_lost = set(lost) - set(reserve)
+# def solution(n, lost, reserve):
+#     #1. 여벌 있는데 도난당항 학생들은 양쪽 명단에서 삭제
+#     real_reserve = set(reserve) - set(lost)
+#     real_lost = set(lost) - set(reserve)
     
-    # 2. set을 list로 바꾸고 정렬 (작은 번호부터 처리해야 최적)
-    real_reserve = sorted(list(real_reserve))
-    real_lost = sorted(list(real_lost))
+#     #2. 다시 리스트로, 작은 번호부터 정렬되도록
+#     real_reserve = sorted(list(real_reserve))
+#     real_lost = sorted(list(real_lost))
     
-    # 3. 진짜 잃어버린 학생 수만큼 일단 빼고 시작 (체육복 있는 학생 수)
-    answer = n - len(real_lost)
+#     #3. 초기 answer
+#     answer = n - len(real_lost)
     
-    # 4. 빌려주기 로직
-    for l in real_lost:
-        if l - 1 in real_reserve:
-            real_reserve.remove(l - 1)
+#     #4. 도난 당한 학생 빌려주기
+#     for l in real_lost:
+#         if l - 1 in real_reserve:
+#             real_reserve.remove(l - 1)
+#             answer += 1
+#         elif l + 1 in real_reserve:
+#             real_reserve.remove(l + 1)
+#             answer += 1
+                
+#     return answer
+
+
+
+
+def solution(n,lost,reserve):
+    #여벌의 체육복을 가진 학생들은, 바로 앞 뒤 학생에게만 빌려줄 수 있으며, 체육복은 최대 2개임
+    #여벌 체육복을 가진 학생도 도난을 당할 수 있음.
+    #체육복을 받아 체육수업을 들을 수 있는 최대 학생 수를 리턴
+    #이거는, 일단 가지고 있는 체육복 수를 체크하는게 맞을 듯
+    get_dict = {}
+    for i in range(n):
+        get_dict[i] = 1
+        if i+1 in lost:
+            get_dict[i] -= 1
+        if i+1 in reserve:
+            get_dict[i] += 1
+    
+    for i in range(n):
+        if get_dict[i] == 0:
+            if i-1 >= 0 and get_dict[i-1] == 2:
+                get_dict[i] += 1
+                get_dict[i-1] -= 1
+            elif i+1 < n and get_dict[i+1] == 2:
+                get_dict[i] += 1
+                get_dict[i+1] -= 1
+    answer = 0
+    for key, value in get_dict.items():
+        if value >= 1:
             answer += 1
-        elif l + 1 in real_reserve:
-            real_reserve.remove(l + 1)
-            answer += 1
-            
     return answer
+        
+        
+    
